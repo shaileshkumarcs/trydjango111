87ecc95571c252055223f2f937ad2831f42b4426
shailesh <shailesh.kumarcs@gmail.com>
2018-05-13 00:58:06 +0530

    restaurants model created

--- restaurants/views.py
+++ restaurants/views.py
@@ -2,6 +2,7 @@ import random
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 
 # Create your views here.
 
@@ -36,16 +37,24 @@ def contact(request):
     }
     return render(request, "contact.html", context)  # response
 
+class HomeView(TemplateView):
+    template_name = 'home.html'
 
-class ContactView(View):
-    def get(self, request, *args, **kwargs):
-        context = {}
-        return render(request, "contact.html", context)  # response
+    def get_context_data(self, *args, **kwargs):
+        context =super(HomeView,self).get_context_data(*args, **kwargs)
+        #print(context)
+        num = None
+        some_list = [
+            random.randint(0, 10000000000),
+            random.randint(0, 10000000000),
+            random.randint(0, 10000000000)
+        ]
+        condition_bool_item = True
+        if condition_bool_item:
+            num = random.randint(0, 1000000000)
+        context = {
+            "num": num,
+            "some_list": some_list
+        }
+        return context
 
-    # def post(self, request, *args, **kwargs):
-    #     context = {}
-    #     return render(request, "contact.html", context)  # response
-    #
-    # def put(self, request, *args, **kwargs):
-    #     context = {}
-    #     return render(request, "contact.html", context)  # response
