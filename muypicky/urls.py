490267af30c590c95fce32f785552b055b03b229
shailesh <shailesh.kumarcs@gmail.com>
2018-05-16 23:23:58 +0530

    queryset completed

--- muypicky/urls.py
+++ muypicky/urls.py
@@ -17,12 +17,13 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from restaurants.views import HomeView
+from restaurants.views import restaurants_listview
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', HomeView.as_view()),
+    url(r'^$', TemplateView.as_view(template_name = 'home.html')),
+    url(r'^restaurants-list/$', restaurants_listview),
     url(r'^about/$', TemplateView.as_view(template_name = 'about.html')),
     url(r'^contact/$', TemplateView.as_view(template_name = 'contact.html')),
     #url(r'^contact/(?P<id>\d+)/$', ContactView.as_view()),
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,60 +1,19 @@
-import random
+# import random
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
 
+from .models import RestaurantLocation
+
 # Create your views here.
 
 #function based view
 
-def home(request):
-    num = None
-    some_list = [
-        random.randint(0,10000000000),
-        random.randint(0,10000000000),
-        random.randint(0,10000000000)
-    ]
-    condition_bool_item = True
-    if condition_bool_item:
-        num = random.randint(0,1000000000)
+def restaurants_listview(request):
+    template_name = 'restaurants/restaurants_list.html'
+    queryset = RestaurantLocation.objects.all()
     context = {
-        "num":num,
-        "some_list":some_list
+        "object_list" : queryset
     }
-    return render(request,"home.html",context)#response
-
-def about(request):
-
-    context = {
-
-    }
-    return render(request,"about.html",context)#response
-
-def contact(request):
-    context = {
-
-    }
-    return render(request, "contact.html", context)  # response
-
-class HomeView(TemplateView):
-    template_name = 'home.html'
-
-    def get_context_data(self, *args, **kwargs):
-        context =super(HomeView,self).get_context_data(*args, **kwargs)
-        #print(context)
-        num = None
-        some_list = [
-            random.randint(0, 10000000000),
-            random.randint(0, 10000000000),
-            random.randint(0, 10000000000)
-        ]
-        condition_bool_item = True
-        if condition_bool_item:
-            num = random.randint(0, 1000000000)
-        context = {
-            "num": num,
-            "some_list": some_list
-        }
-        return context
-
+    return render(request, template_name, context)
