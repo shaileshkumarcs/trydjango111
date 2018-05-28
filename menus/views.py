e5de8f89c5bcb929d86cdbffa0e8d61ce1fba46e
shailesh <shailesh.kumarcs@gmail.com>
2018-05-28 23:02:38 +0530

    Only View created and form will creat in next

--- menus/views.py
+++ menus/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 # Create your views here.
+from .models import Item
+
+class ItemListView(ListView):
+
+    def get_queryset(self):
+        return Item.objects.filter(user=self.request.user)
+
+
+class ItemDetailView(DetailView):
+
+    def get_queryset(self):
+        return Item.objects.filter(user=self.request.user)
+
+
+class ItemCreateView(CreateView):
+
+    def get_queryset(self):
+        return Item.objects.filter(user=self.request.user)
+
+
+class ItemUpdateView(UpdateView):
+
+    def get_queryset(self):
+        return Item.objects.filter(user=self.request.user)
