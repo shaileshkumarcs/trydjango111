2146cbdb136e60a385d93199cb315cdaeba6f041
shailesh <shailesh.kumarcs@gmail.com>
2018-05-29 08:38:01 +0530

    Item list view created and forms as well as

--- menus/models.py
+++ menus/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.core.urlresolvers import reverse
 
 from restaurants.models import RestaurantLocation
 
@@ -8,20 +9,24 @@ from restaurants.models import RestaurantLocation
 class Item(models.Model):
     # associations
     user        = models.ForeignKey(settings.AUTH_USER_MODEL)
-    restaurants = models.ForeignKey(RestaurantLocation)
+    restaurant = models.ForeignKey(RestaurantLocation)
     # Item stuff
     name        = models.CharField(max_length=120)
-    contants    = models.TextField(help_text='Separated each item by comma')
+    contents    = models.TextField(help_text='Separated each item by comma')
     excludes    = models.TextField(blank=True,null=True, help_text='separated each item by comma')
     public      = models.BooleanField(default=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
     updated     = models.DateTimeField(auto_now=True)
 
+    def get_absolute_url(self): #get_absolute_url
+        #return f"/restaurants/{self.slug}" #this is for hard code url pattern
+        return reverse("menus:detail", kwargs={'pk':self.pk})
+
     class Meta:
         ordering = ['-updated','-timestamp']
 
     def get_contents(self):
-        return self.contants.split(",")
+        return self.contents.split(",")
 
     def get_excludes(self):
         return self.excludes.split(",")
--- muypicky/urls.py
+++ muypicky/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name = 'home.html'), name="home"),
     url('^login/$', LoginView.as_view(), name='login'),
     #url('^password_reset_view/$', PasswordResetView.as_view(), name='password_reset'),
+    url(r'^items/', include('menus.urls', namespace='menus')),
     url(r'^restaurants/', include('restaurants.urls', namespace='restaurants')),
     url(r'^about/$', TemplateView.as_view(template_name = 'about.html'), name="about"),
     url(r'^contact/$', TemplateView.as_view(template_name = 'contact.html'), name="contact"),
